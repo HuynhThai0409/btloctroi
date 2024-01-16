f5c1c0408f22290a9c0682b35223e7cbf422147a
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-01-16 22:45:37 +0700

    Upload source code

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -42,7 +42,7 @@ const Saved = () => {
         </ul>
       )} */}
 
-      <Accordion type="single" collapsible className="flex flex-col gap-9 w-full  max-w-5xl">
+      <Accordion type="single" collapsible className="flex flex-col gap-9 w-full max-w-full">
         <AccordionItem value="item-1">
           <AccordionTrigger>Các vấn đề thường gặp khi vận hành máy kiểm code KT07</AccordionTrigger>
           <AccordionContent>
