c207410cf40cd46c6a95a67e7cdc2f3deb9d9b6f
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-01-16 16:20:42 +0700

    Upload source code

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,19 +1,19 @@
-import { Models } from "appwrite";
-
-import { GridPostList, Loader } from "@/components/shared";
-import { useGetCurrentUser } from "@/lib/react-query/queries";
+//import { Models } from "appwrite";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+// import { GridPostList, Loader } from "@/components/shared";
+// import { useGetCurrentUser } from "@/lib/react-query/queries";
 
 const Saved = () => {
-  const { data: currentUser } = useGetCurrentUser();
+  //const { data: currentUser } = useGetCurrentUser();
 
-  const savePosts = currentUser?.save
-    .map((savePost: Models.Document) => ({
-      ...savePost.post,
-      creator: {
-        imageUrl: currentUser.imageUrl,
-      },
-    }))
-    .reverse();
+  // const savePosts = currentUser?.save
+  //   .map((savePost: Models.Document) => ({
+  //     ...savePost.post,
+  //     creator: {
+  //       imageUrl: currentUser.imageUrl,
+  //     },
+  //   }))
+  //   .reverse();
 
   return (
     <div className="saved-container">
@@ -25,10 +25,10 @@ const Saved = () => {
           alt="edit"
           className="invert-white"
         />
-        <h2 className="h3-bold md:h2-bold text-left w-full">Đã lưu</h2>
+        <h2 className="h3-bold md:h2-bold text-left w-full">Bảng tin bảo trì</h2>
       </div>
 
-      {!currentUser ? (
+      {/* {!currentUser ? (
         <Loader />
       ) : (
         <ul className="w-full flex justify-center max-w-5xl gap-9">
@@ -38,7 +38,18 @@ const Saved = () => {
             <GridPostList posts={savePosts} showStats={false} />
           )}
         </ul>
-      )}
+      )} */}
+
+      <Accordion type="single" collapsible className="flex gap-2 w-full max-w-5xl">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Các vấn đề thường gặp khi vận hành máy kiểm code KT07</AccordionTrigger>
+          <AccordionContent>
+            Chiều về là lúc bến sông quê tôi tấp nập nhất. Đoàn thuyền chở các bà, các chị từ chợ huyện, chợ tỉnh về cập bến. Các bà các chị được đàn con ùa ra đón. Con lớn đỡ cho mẹ gánh hàng. Con nhỏ vòi mẹ chia quà. Tiếng cười nói rộn ràng cả một khúc sông. Rồi ai về nhà nây. Con thuyền neo vào bến đỗ. Đây cũng là lúc bọn trẻ chăn trâu lùa trâu xuống tắm. Bọn trẻ tắm cho trâu, rồi bọn trẻ giỡn nước. Chúng té nước cho nhau. Chúng chơi trò đánh trận. Một đứa kiếm đâu được trái bóng tròn. Thế là chúng ném bóng cho nhau. Một ý kiến được cả bọn chấp nhận: chơi bóng nước. Chúng chia làm hai phe, chuyền bóng cho nhau. Phe nào chuyền được 6 chuyền là thắng. Phe thua phải cõng phe thắng chạy dọc con sông suốt từ bến tắm đến tận gốc đa. Bến sông quê tôi cứ rộn ràng như vậy cho đến lúc mặt trời lặn phía chân ười mới có chút bình lặng.
+            <img src="/assets/img-doc/kaizen.jpg" alt="" />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
     </div>
   );
 };
