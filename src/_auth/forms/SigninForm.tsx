b1e985f9bc4b0addd87fb8ef27a9dab0dbd40204
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-01-18 16:43:40 +0700

    Upload source code

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -54,10 +54,9 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        {/* <img src="/assets/images/logo.png" alt="logo" width={200} height={50}/> */}
+        <img src="/assets/images/logo.png" alt="logo" width={200} height={28}/>
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
-          {/* Đăng nhập */}<img src="/assets/images/logo.png" alt="logo" width={200} height={50}/> <br />
           Đăng nhập
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
