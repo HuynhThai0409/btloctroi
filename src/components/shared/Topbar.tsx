3f2d4385ddbdbf38d8999d68f7a023ffc8e6260b
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-01-18 10:53:41 +0700

    Upload source code

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -21,7 +21,7 @@ const Topbar = () => {
           <img
             src="/assets/images/logo.png"
             alt="logo"
-            width={100} //130
+            width={130} //130
             height={250} //325
           />
         </Link>
