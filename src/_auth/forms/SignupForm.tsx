9084f8bac4b29b46f40c47096dca7a4783835b73
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-01-21 09:30:05 +0700

    Upload source code

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -75,7 +75,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.png" alt="logo" width={170} height={36}/>
+        {/* <img src="/assets/images/logo.png" alt="logo" width={170} height={36}/> */}
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Tạo tài khoản
