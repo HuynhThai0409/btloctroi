486453f07e2a0d16fbb849f8249e678b48a1b82a
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-01-21 09:14:29 +0700

    Upload source code

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -16,7 +16,7 @@ export default function AuthLayout() {
           </section>
 
           <img
-            src="/assets/images/BAOTRI.png"
+            src="/assets/images/logo.svg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
