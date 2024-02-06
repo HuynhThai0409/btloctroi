c466567e1b7a334ad49bf2b5dfefa39eeb626873
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-02-06 15:31:25 +0700

    Upload source code

--- src/_root/pages/CheckClassify.tsx
+++ src/_root/pages/CheckClassify.tsx
@@ -4,14 +4,12 @@ import React from "react"
 const CheckClassify = () => {
     const [date, setDate] = React.useState<Date | undefined>(new Date())
     return (
-        <div>
-            <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                className="rounded-md border shadow"
-            />
-        </div>
+        <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-md border shadow"
+        />
     )
 }
 
