aaa9c3171d816064da556b2e4d554d0929499579
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-02-16 08:16:42 +0700

    Upload source code

--- src/_root/pages/CheckClassify.tsx
+++ src/_root/pages/CheckClassify.tsx
@@ -1,6 +1,17 @@
 const CheckClassify = () => {
     return (
-        <div>Data Explore</div>
+        <div className="wrapper">
+            <h3>Upload & View Excel Sheets</h3>
+            {/* form */}
+            <form className="form-group custom-form">
+                <input type="file" className="form-control" required />
+                <button type="submit" className="btn btn-success btn-md">Upload</button>
+            </form>
+            {/* view data */}
+            <div className="viewer">
+                View Data
+            </div>
+        </div>
     )
 }
 
