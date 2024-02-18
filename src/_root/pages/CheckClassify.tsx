d1e87f4796c882c6277256b55354a71c75812551
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-02-18 10:51:48 +0700

    Upload source code

--- src/_root/pages/CheckClassify.tsx
+++ src/_root/pages/CheckClassify.tsx
@@ -16,12 +16,12 @@ const CheckClassify = () => {
             <div className="common-container">
                 <div className="max-w-5xl flex-start gap-3 justify-start w-full">
                     <img
-                        src="/assets/icons/add-post.svg"
+                        src="/assets/icons/filter.svg"
                         width={36}
                         height={36}
                         alt="add"
                     />
-                    <h2 className="h3-bold md:h2-bold text-left w-full">Tải lên</h2>
+                    <h2 className="h3-bold md:h2-bold text-left w-full">Phân loại và so sánh dữ liệu</h2>
                 </div>
             </div>
         </div>
