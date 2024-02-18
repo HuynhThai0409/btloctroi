16084e6824309ae1b3d93f653a07e219da8b9e67
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-02-18 14:51:31 +0700

    Upload source code

--- src/_root/pages/CheckClassify.tsx
+++ src/_root/pages/CheckClassify.tsx
@@ -23,20 +23,19 @@ const CheckClassify = () => {
                     />
                     <h2 className="h3-bold md:h2-bold text-left w-full">Phân loại và so sánh dữ liệu</h2>
                 </div>
-                <h2 className="rounded-md box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
-                    So sánh dữ liệu
-                </h2>
-                <div className="flex flex-col gap-9 w-full  max-w-5xl">
-                    <h3>Dữ liệu 1:</h3>
-                    <input type="file" className="form-control" required />
+                <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+                    <h2 className="rounded-md box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
+                        So sánh dữ liệu
+                    </h2>
+                    <div className="flex flex-col gap-9 w-full  max-w-5xl">
+                        <h3>Dữ liệu 1:</h3>
+                        <input type="file" className="form-control" required />
+                    </div>
+                    <div className="flex flex-col gap-9 w-full  max-w-5xl">
+                        <h3>Dữ liệu 2:</h3>
+                        <input type="file" className="form-control" required />
+                    </div>
                 </div>
-                <div className="flex flex-col gap-9 w-full  max-w-5xl">
-                    <h3>Dữ liệu 2:</h3>
-                    <input type="file" className="form-control" required />
-                </div>
-                <h2 className="rounded-md box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
-                    Phân loại dữ liệu
-                </h2>
             </div>
         </div>
     )
