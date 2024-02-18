2c13c666bd0556a046192174dd58e2f25698094d
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-02-18 10:49:19 +0700

    Upload source code

--- src/_root/pages/CheckClassify.tsx
+++ src/_root/pages/CheckClassify.tsx
@@ -1,18 +1,31 @@
 const CheckClassify = () => {
     return (
-        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
-            <h3>Upload & View Excel Sheets</h3>
-            {/* form */}
-            <form className="form-group custom-form">
-                <input type="file" className="form-control" required />
-                <button type="submit" className="btn btn-success btn-md">Upload</button>
-            </form>
-            {/* view data */}
-            <div className="viewer">
-                View ata
+        // <div className="wrapper">
+        //     <h3>Upload & View Excel Sheets</h3>
+        //     {/* form */}
+        //     <form className="form-group custom-form">
+        //         <input type="file" className="form-control" required />
+        //         <button type="submit" className="btn btn-success btn-md">Upload</button>
+        //     </form>
+        //     {/* view data */}
+        //     <div className="viewer">
+        //         View ata
+        //     </div>
+        // </div>
+        <div className="flex flex-1">
+            <div className="common-container">
+                <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+                    <img
+                        src="/assets/icons/add-post.svg"
+                        width={36}
+                        height={36}
+                        alt="add"
+                    />
+                    <h2 className="h3-bold md:h2-bold text-left w-full">Tải lên</h2>
+                </div>
             </div>
         </div>
     )
 }
- 
+
 export default CheckClassify
