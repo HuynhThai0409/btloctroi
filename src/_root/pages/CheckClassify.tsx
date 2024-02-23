e5621fe579b4cb7887cd6e4d145fe457a4d579db
HuynhThai0409 <125648924+HuynhThai0409@users.noreply.github.com>
2024-02-23 10:05:55 +0700

    Upload source code

--- src/_root/pages/CheckClassify.tsx
+++ src/_root/pages/CheckClassify.tsx
@@ -55,15 +55,15 @@ const CheckClassify = () => {
                     <h2 className="h3-bold md:h2-bold text-left w-full">Phân loại và so sánh dữ liệu</h2>
                 </div>
             </div>
-            <div className="wrapper">
+            <div className="h3-bold md:h2-bold text-left w-full">
                 <h3>Upload & View Excel Sheets</h3>
 
-                <form>
-                    <input type="file" required />
-                    <button type="submit">Upload</button>
+                <form className="form-group custom-form">
+                    <input type="file" className="form-control" required />
+                    <button type="submit" className="" >Upload</button>
                 </form>
 
-                <div>
+                <div className="viewer">
                     View Data
                 </div>
 
